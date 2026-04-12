7ec07366f3dbdbef989e79909c1d3590b2a84541
sajan <bhatsajan2005@gmail.com>
2026-04-13 03:42:03 +0530

    added p1_2

--- Day2/d2p1.cpp
+++ Day2/d2p1.cpp
@@ -1,1 +1,38 @@
-//This is Day 2
+//Star Patterns
+
+#include <iostream>
+
+using namespace std;
+void square(){
+        int n=5;
+        for ( int i=1; i<=n; i++){
+            for(int j=1; j<=n; j++){
+                cout << " * ";
+            } 
+            cout << endl; 
+        }
+    }
+
+
+void triangle(){
+    int n=5;
+    for (int i=1; i <=n ; i++){
+        for(int j=1; j<=i; j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+}
+
+
+int main(){
+
+    cout<< "SQUARE"<<endl;
+    square();
+
+    cout<<endl;
+
+    cout<< "TRIANGLE"<<endl;
+    triangle();
+    return 0;
+}
