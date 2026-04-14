84fb02ee4049432dcfee7ed09f20ed87de084e0e
sajan <bhatsajan2005@gmail.com>
2026-04-15 00:03:07 +0530

    updated bank

--- Day2/d2p6.cpp
+++ Day2/d2p6.cpp
@@ -1,74 +1,78 @@
 #include <iostream>
 
 using namespace std;
+float debit( float amt, float bal){
 
-void debit( float deb,float current_bal){
-    if(current_bal-deb<500){
-        cout << "Minimum Balance is needed in Account." <<endl;
-    }else{
-        current_bal=current_bal-deb;
-        cout << "Balance Updated" << endl;
-}
-}
-float credit(float current_bal, float amt){
-    if (amt>1000000){
-        cout << "Must be less than 10 Lakhs." << endl;
-        cout << "Transaction Failed..." << endl;
+    if (bal-amt < 500){
+        cout << "\n Minimum balance Required... \n";
     }
     else{
-        current_bal=current_bal+amt;
-        cout << "Balance Updated" << endl;
+        bal = bal-amt;
+        cout << amt << ".RS Debited Successfully \n";
     }
-    return current_bal;
-
+    return bal;
 }
 int main(){
-    float password=1234;
-    float balance=9000.00;
-    float current_bal=1000;
-    int option=0;
-    
-
-    while ( option != 4){
-        cout << "_______MENU_______" << endl;
-        cout << "1. DEBIT AMOUNT" << endl;
-        cout << "2. CREDIT AMOUNT" <<endl;
-        cout << "3. CHECK BALANCE" <<endl;
-        cout << "4. EXIT" <<endl;
-        cout << "ENTER YOUR CHOICE :" <<endl;
+    int count=0;
+    int option;
+    double balance=5000;
+    int acc_no=1234;
+    int psrd=1234;
+    int accno,pass;
+    do{
+        cout << "------------------WELCOME------------------ \n";
+    cout << "\n \n ACCOUNT NUMBER: \n";
+    cin >>accno;
+    cout << "PASSWORD: \n";
+    cin >> pass;
+    if (accno== acc_no and pass==psrd){
+    do{
+        cout << "_____MENU______ \n";
+        cout << "1.CREDIT \n";
+        cout << "2.DEBIT \n";
+        cout << "3.CHECK BALANCE \n";
+        cout << "4.EXIT \n";
+        cout << "Choose an option: \n";
         cin >> option;
         switch (option)
         {
         case 1:
-            float debit_amount;
-            cout<< "Enter amount you want to debit:" << endl;
-            cin>>debit_amount;
-            debit(debit_amount,current_bal);
-            cout << current_bal <<endl;
+            cout << "CURRENTLY THIS OPTION IS NOT AVALABLE...... \n";
+            cout << "Please Visit Your Nearest Bank"<<endl;
             break;
         
         case 2:
-            float credit_amt;
-            cout << "Enter the amount you want to add in machine..."<<endl;
-            cin>>credit_amt;
-            if (credit_amt<1 ){
-                cout << "Must be more" << endl;
-            }
-            else
-            {
-                float credit_amt=credit(credit_amt,current_bal);
-                cout << "Rs. "<<credit_amt<< " Credited Successfully"<<endl;
-            }
+            float deb_amt;
+            cout << "Enter the amount you want to debit:";
+            cin>>deb_amt;
+            char confirm;
+            cout << "ARE YOU SURE (Y/N): \n";
+            cin >> confirm;
+            if(confirm=='Y' || confirm == 'y'){
+                balance=debit(deb_amt,balance);
+            } 
             break;
         case 3:
-            cout << "________BALANCE__________" <<endl;
-            cout << "CURRENT BALANCE: "<<current_bal <<endl;
+            cout << "___________________________________ \n";
+            cout << "CURRENT BALANCE :"<< balance <<endl;
+            cout << "___________________________________ \n";
             break;
         case 4:
-            cout<< "Exiting..." <<endl;
-            cout <<"Thank You Visit Again....."<<endl;
+            cout << "EXITING.............." << endl;
+            cout << "THANK YOU VISIT AGAIN" <<endl;
+            count =10;
+            break;
+        default:
+            cout << "Invalid Choice" <<endl;
             break;
         }
+    }while ( option!=4);
     }
+    else{
+        cout << "INCORRECT ACC NO OR PASSWORD \n";
+        cout << "Please Try AGAIN.....";
+        count ++;
+    }
+    }while (count <5);
     return 0;
 }
