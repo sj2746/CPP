4311d8e771062ee6e330e03d48be88d66cc2e629
sajan <bhatsajan2005@gmail.com>
2026-04-17 00:14:57 +0530

    updated

---
+++ Day5/d5p2.cpp
@@ -0,0 +1,10 @@
+//Travesring Array
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={17,28,18,90,75};
+    for(int i=0;i<5;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
