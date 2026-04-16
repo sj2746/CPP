7200153bddc11db2ec5665f5a21364014be230d9
sajan <bhatsajan2005@gmail.com>
2026-04-16 23:48:18 +0530

    updated

--- Day4/d4p1.cpp
+++ Day4/d4p1.cpp
@@ -1,11 +1,10 @@
-# include <iostream>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
-
-    int arr[15]={2,3,7,8,1,81,982,15,28,18,92,162,81};
-    cout << arr[8];
-
-    return 0;
+   vector<int> nums = {1,2,3};
+    cout << nums[1];
+   return 0;
 }
