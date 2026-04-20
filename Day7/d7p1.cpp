c4cd85e4f1f1027f2b5262aac467fbc8e351111a
sajan <bhatsajan2005@gmail.com>
2026-04-20 23:31:11 +0530

    added

---
+++ Day7/d7p1.cpp
@@ -0,0 +1,12 @@
+//Arrays
+#include <iostream>
+using namespace std;
+int main(){
+
+    int arr[]={1,2,3,4,5};
+    cout << arr[0];
+    for(int i=0;i<5;i++){
+        cout <<endl<<  arr[i] <<endl;
+    }
+    return 0;
+}
---
+++ Day7/d7p2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,2,3,4,5};
+    int maxi=arr[0];
+    for(int i=1;i<4;i++){
+        if (arr[i]>maxi) maxi=arr[i];
+    }
+    return 0;
+}
