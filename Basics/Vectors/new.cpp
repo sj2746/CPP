1dfbda08825a04636d07740be76b87d3d37b0fc0
sajan <bhatsajan2005@gmail.com>
2026-04-24 23:46:07 +0530

    updated

---
+++ Basics/Vectors/new.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int a=65;
+    int b=75;
+    int c=83;
+    int d=72;
+
+    cout << char(a) <<char(b)<<char(c)<<char(d);
+    return 0;
+}
