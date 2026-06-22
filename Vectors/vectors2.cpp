880fad2ebe555dc2c077e567c6f0fa7a0fd7cf51
sajan <bhatsajan2005@gmail.com>
2026-06-23 05:20:56 +0530

    updated

--- Vectors/vectors2.cpp
+++ Vectors/vectors2.cpp
@@ -1,13 +1,13 @@
 #include<iostream>
 #include<bits/stdc++.h>
-
+//This is a function
 using namespace std;
 int main(){
     vector<int> v;
     v={10,60,40,20,97,35,47};
-
-   
-    return 0;
-    
-    
+    v.push_back(76);
+    v.pop_back();
+    for(int i:v){
+        cout << i << " ";
+    }
 }
