dd766e0c035ca027697049c274e53b6e7d058870
sajan <bhatsajan2005@gmail.com>
2026-06-23 05:45:49 +0530

    added

---
+++ Basics/user_details.cpp
@@ -0,0 +1,16 @@
+//Program 2
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    char name[20],city[20];
+    cout<<"Enter the name: ";
+    cin>>name;
+    cout<<endl<<"Enter age :";
+    cin>>age;
+    cout<<endl<<"Enter City :";
+    cin>>city;
+    cout<<"The user details :"<<endl;
+    cout<<"Name :"<<name<<endl<<"Age :"<<age<<endl<<"City :"<<city;
+    return 0;
+}
