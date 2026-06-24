7a8bf940bb4b3f3bbb3c733c6c2b163f95fc82f7
sajan <bhatsajan2005@gmail.com>
2026-06-24 06:13:46 +0530

    added

---
+++ Patterns/right_triangle_inverted.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=5;
+    //For DESCREASING TRIANGLE same but only change No of rows From Increasing to Decresing order
+    cout<<"Descreasing Right Triangle :"<<endl;
+    for(int i=n;i>=1;i--){
+        for(int j=1;j<=i;j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
