2a418e8085f774710ecfe0f168bc218ca0b8893d
sajan <bhatsajan2005@gmail.com>
2026-06-26 20:50:38 +0530

    added

---
+++ Basics/arrays/swap_zeros.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[]={1,0,2,0,3,4,0};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int j=0;
+    int temp;
+    for(int i=0;i<n;i++){
+        if(arr[i]!=0){
+            temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            j++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
