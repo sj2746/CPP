bdbc31aa0352fdfb654b4606f834cb9c787f6fa5
sajan <bhatsajan2005@gmail.com>
2026-06-27 19:51:01 +0530

    added

---
+++ Basics/arrays/remove_duplicates.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int arr[]={1,1,2,2,3,4,4,5};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    
+    for(int i=0;i<n;i++){
+        if
+    }
+    return 0;
+}
