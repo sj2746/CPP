ae6fbb11483cf9f09fe62e82cbce781d8fdc9ae1
sajan <bhatsajan2005@gmail.com>
2026-06-27 20:19:54 +0530

    added

--- Basics/arrays/remove_duplicates.cpp
+++ Basics/arrays/remove_duplicates.cpp
@@ -4,9 +4,16 @@ int main(){
 
     int arr[]={1,1,2,2,3,4,4,5};
     int n=sizeof(arr)/sizeof(arr[0]);
-    
-    for(int i=0;i<n;i++){
-        if
+    int j=1;
+    for(int i=1;i<n;i++){
+        if(arr[i]!=arr[i-1]){
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    int nums=j;
+    for(int i=0;i<nums;i++){
+        cout<<arr[i]<<" ";
     }
     return 0;
 }
