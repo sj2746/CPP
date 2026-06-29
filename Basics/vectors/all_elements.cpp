450031ec6ca60b91540605521bf6f89f62cb86e6
sajan <bhatsajan2005@gmail.com>
2026-06-29 19:54:49 +0530

    added

---
+++ Basics/vectors/all_elements.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec = {1, 2, 3};
+    //vector<int> vec(3,0)
+    for(int i=0;i<=2;i++)
+    cout<<vec[i]<<" ";
+}
